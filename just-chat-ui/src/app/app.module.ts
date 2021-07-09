6fe95655b35b6737ae9ecc3b593f13fb719442d7
Suraj Gharat <mr.surajgharat2@gmail.com>
2021-07-09 08:40:42 +0530

    Create new angular project for chat ui

---
+++ just-chat-ui/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SigninComponent } from './signin/signin.component';
+import { ChatListComponent } from './chat-list/chat-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SigninComponent,
+    ChatListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
