343eb6585ec5346ae8a3baeed2eb02b5e02e41e5
Suraj Gharat <mr.surajgharat2@gmail.com>
2021-07-10 18:30:03 +0530

    Learn to add angular form module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SigninComponent } from './signin/signin.component';
-import { ChatListComponent } from './chat-list/chat-list.component';
+import { ChatListComponent } from './chat-list/chat-list.component'
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { ChatListComponent } from './chat-list/chat-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms'
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
+  //   template: `
+  //   Favorite Color: <input type="text" [formControl]="username2">
+  // `,
   styleUrls: ['./signin.component.css']
 })
 export class SigninComponent implements OnInit {
 
+  userDetails = new FormGroup({
+    username: new FormControl(''),
+    displayName: new FormControl()
+  })
+
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+  onSubmit() {
+    console.warn(this.userDetails.value)
   }
 
 }
