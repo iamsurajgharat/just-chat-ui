29706129833e3091028a48d2a670d9cae9dc0116
iamsurajgharat <mr.surajgharat2@gmail.com>
2021-08-18 21:30:54 +0530

    Add basic websocket client code

--- src/app/services/backend.service.ts
+++ src/app/services/backend.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { UserProfile } from '../models/user-profile';
 import { BaseMessage } from '../models/ws-messages';
+import {webSocket, WebSocketSubject} from 'rxjs/webSocket'
+
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +15,21 @@ export class BackendService {
   // actions we need to support
   // connect
   connect(user: UserProfile): ConnectResponse {
+
+    const webSocketSubject = webSocket("ws://localhost:8081/socket3")
+    webSocketSubject.subscribe({
+      next(x) { console.log('got value ' + x); },
+      error(err) { console.error('something wrong occurred: ' + err); },
+      complete() { console.log('we socket done'); }
+    });
+    webSocketSubject.next({userId: 'some message', name:'Suraj Gharat'})
+    webSocketSubject.next({message: {userId: 'some message', name:'Suraj Gharat'}});
+
+
+    //webSocketSubject.complete(); // Closes the connection.
+ 
+    //webSocketSubject.error({code: 4000, reason: 'I think our app just broke!'});
+
     // try to establish ws connection
     return {
       isSuccess: true,
