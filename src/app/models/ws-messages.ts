3feb48178b514b8ac32b855867ca2962a9c14cb8
iamsurajgharat <mr.surajgharat2@gmail.com>
2021-09-02 09:40:48 +0530

    Update serialization _type names as per backend changes

--- src/app/models/ws-messages.ts
+++ src/app/models/ws-messages.ts
@@ -4,8 +4,8 @@ import { nonEmpty } from "../utility/common-util"
 import { ChatMessage } from "./chat-message"
 
 export const PropertyName_Type = '_type'
-export const TypeName_ResponseConnected = 'actors.UserSessionActor2.Connected'
-export const TypeName_ResponsePinnedChats = 'actors.UserSessionActor2.PinnedChats'
+export const TypeName_ResponseConnected = 'actors.UserSessionActor.Connected'
+export const TypeName_ResponsePinnedChats = 'actors.UserSessionActor.PinnedChats'
 export const TypeName_ResponseSingleChat = 'models.SingleChat'
 export const TypeName_ResponseGroupChat = 'models.GroupChat'
 export const TypeName_ResponseUserProfile = 'models.UserProfile'
@@ -74,7 +74,7 @@ export interface BaseOutgoingMessage extends BaseMessage {
 }
 
 export class ConnectRequest implements BaseOutgoingMessage {
-    _type = 'actors.UserSessionActor2.Connect'
+    _type = 'actors.UserSessionActor.Connect'
     constructor(public userId: string, public name: string) { }
 }
 
